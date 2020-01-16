77aa6b2e8a1c78979724d36fa3bec679113320c0
Pedro Antonio Gamez <48693687+gamezpedro@users.noreply.github.com>
2020-01-16 08:33:01 -0600

    Creating JavaScript File for Button Functioning

---
+++ Parte-3/index.js
@@ -0,0 +1,14 @@
+submit = $('submit');
+crowdname = document.getElementById("name").
+
+function SubmitListener(){
+    $(submit).on("click",function(e){
+
+    });
+}
+
+
+function init(){
+    submitListener();
+}
+init();
